e75915d36168213310fb6ff9983e595e045d6ddf
chingizshortanov1232 <111279993+chingizshortanov1232@users.noreply.github.com>
2022-08-23 16:27:59 +0600

    Add files via upload

---
+++ Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+int numberA = new Random().Next(1, 100); 
+Console.WriteLine("Первое чило: " + numberA );
+
+int numberB = new Random().Next(1, 100);
+Console.WriteLine("Второе число: " + numberB );
+
+Console.Write("Является ли первое число квадратом второго? ");
+
+if ( (numberA / numberB) != numberB)
+        {
+            Console.WriteLine ("Нет");
+        }
+else
+        {
+            Console.WriteLine ("Да");
+        }
